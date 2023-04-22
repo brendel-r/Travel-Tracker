82477d9870205ab6af52ece190e5093c4765ce71
Rachel Brendel <brendelrachel@gmail.com>
2023-04-22 14:17:58 -0500

    Build basic setup for fetching api data

--- src/scripts.js
+++ src/scripts.js
@@ -1,9 +1,31 @@
-
-// An example of how you tell webpack to use a CSS (SCSS) file
+import { fetchAllData } from '../src/apiCalls';
 import './css/styles.css';
-
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/travel-wizard.png'
+import  Traveler from './Traveler' 
+import  Destination from './Destination'
+import  Trip from './Trip' 
+// import dayjs from 'dayjs'
 
 
-console.log('This is the JavaScript entry file - your code begins here.');
+// Queury Selectors
+// const welcomeUserDisplay = document.getElementById('welcome-user'),
+//       totalSpentDisplay = document.getElementById('total-spent'),
+//       upcomingTripsDisplay = document.getElementById('upcoming-trips'),
+//       pastTripsDisplay = document.getElementById('past-trips');
+
+const destinations = [];
+
+
+let fetches = fetchAllData();
+fetches.then(([travelerData, tripData, destinationData]) => {
+  console.log(travelerData, tripData, destinationData)
+  tripData.trips.forEach(aTrip => 
+    new Trip(aTrip))
+  travelerData.travelers.forEach(aTraveler => {
+    new Traveler(aTraveler)
+  })
+
+  destinationData.destinations.forEach(aDestination => 
+    destinations.push(new Destination(aDestination)))
+})
+
