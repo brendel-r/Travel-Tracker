0b6424cae236108e4b4932a5d1635cf54bd564fa
Rachel Brendel <brendelrachel@gmail.com>
2023-04-22 16:08:24 -0500

    commented line, removed desinations array

--- src/scripts.js
+++ src/scripts.js
@@ -1,31 +1,29 @@
 import { fetchAllData } from '../src/apiCalls';
 import './css/styles.css';
 import './images/travel-wizard.png'
-import  Traveler from './Traveler' 
-import  Destination from './Destination'
-import  Trip from './Trip' 
+import Traveler from './Traveler'
+import Destination from './Destination'
+import Trip from './Trip'
 // import dayjs from 'dayjs'
 
 
-Queury Selectors
+// Query Selectors
 const welcomeUserDisplay = document.getElementById('welcome-user'),
-      totalSpentDisplay = document.getElementById('total-spent'),
-      upcomingTripsDisplay = document.getElementById('upcoming-trips'),
-      pastTripsDisplay = document.getElementById('past-trips');
+  totalSpentDisplay = document.getElementById('total-spent'),
+  upcomingTripsDisplay = document.getElementById('upcoming-trips'),
+  pastTripsDisplay = document.getElementById('past-trips');
 
-const destinations = [];
 
 
 let fetches = fetchAllData();
 fetches.then(([travelerData, tripData, destinationData]) => {
-  console.log(travelerData, tripData, destinationData)
-  tripData.trips.forEach(aTrip => 
+  tripData.trips.forEach(aTrip =>
     new Trip(aTrip))
   travelerData.travelers.forEach(aTraveler => {
     new Traveler(aTraveler)
   })
 
-  destinationData.destinations.forEach(aDestination => 
-    destinations.push(new Destination(aDestination)))
+  destinationData.destinations.forEach(aDestination =>
+    new Destination(aDestination))
 })
 
